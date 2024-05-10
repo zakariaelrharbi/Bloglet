26fcf1299ced2e755597aa7ac5e765965365a14b
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-05-11 00:14:28 +0100

    adding logo and search

--- Client/Bloglet/src/components/Header.jsx
+++ Client/Bloglet/src/components/Header.jsx
@@ -1,13 +1,21 @@
-import { Navbar } from 'flowbite-react'
-import React from 'react'
-import {Link} from 'react-router-dom'
+import { Navbar, TextInput } from 'flowbite-react';
+import React from 'react';
+import {Link} from 'react-router-dom';
+import { AiOutlineSearch } from "react-icons/ai";
 
 export default function Header() {
   return (
     <Navbar className='border-b-2'>
-        <Link to="/" className=''>
-          Bloglet
+        <Link to="/" className='px-2 self-center font-Roboto font-bold text-l sm:text-xl dark:text-white'>
+          BloGlet
         </Link>
+        <form>
+          <TextInput
+            type='text'
+            placeholder='Search...'
+            rightIcon={AiOutlineSearch} 
+          />
+        </form>
     </Navbar>
   )
 }

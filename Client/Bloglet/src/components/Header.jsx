be633e426deb3211d38d853af7f2784e878211da
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-05-10 23:40:35 +0100

    creating header components

--- Client/Bloglet/src/components/Header.jsx
+++ Client/Bloglet/src/components/Header.jsx
@@ -1,9 +1,13 @@
+import { Navbar } from 'flowbite-react'
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 export default function Header() {
   return (
-    <div>
-        
-    </div>
+    <Navbar className='border-b-2'>
+        <Link to="/" className=''>
+          Bloglet
+        </Link>
+    </Navbar>
   )
 }

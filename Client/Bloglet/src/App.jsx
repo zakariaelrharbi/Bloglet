5ac6208e22bf1a2e48cdbd1d674e518ce0e11728
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-05-10 17:23:44 +0100

    routes installed

--- Client/Bloglet/src/App.jsx
+++ Client/Bloglet/src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import SignIn from './Pages/SignIn'
+import SignUp from './Pages/SignUp'
+import Dashboard from './Pages/Dashboard'
+import Projects from './Pages/Projects'
 
 const App = () => {
   return (
     <div>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/Sign-in' element={<SignIn/>}/>
+          <Route path='/Sign-Up' element={<SignUp/>}/>
+          <Route path='/dashboard' element={<Dashboard/>}/>
+          <Route path='/projects' element={<Projects/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }

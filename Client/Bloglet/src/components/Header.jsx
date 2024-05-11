091fa8f7f93135e6447b97fe80e46e5943400354
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-05-11 21:53:37 +0100

    bug double <a> fixed

--- Client/Bloglet/src/components/Header.jsx
+++ Client/Bloglet/src/components/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
   const path = useLocation().pathname;
   return (
     <Navbar className='border-b-2'>
-      <Link to="/" className='lg:px-8 mr-14 font-Roboto font-bold text-l sm:text-xl dark:text-white'>
+      <Link to="/" className='lg:px-8 mr-14 font-Roboto font-bold text-2xl lg:text-2xl dark:text-white'>
         BloGlet 
       </Link>
       <div className="flex md:order-2">
@@ -23,13 +23,13 @@ export default function Header() {
       </div>
       <Navbar.Collapse  >
 
-        <div className='lg:flex gap-8 items-center px-[290px]'>
-        <Navbar.Link active={path === "/"}>
+        <div className='lg:flex lg:gap-8 lg:items-center lg:px-[280px]'>
+        <Navbar.Link active={path === "/"} as={'div'}>
           <Link to='/'>Home</Link>
         </Navbar.Link>
-        <Navbar.Link active={path === "/about"}><Link to='/about'>About</Link></Navbar.Link>
-        <Navbar.Link active={path === "/projects"}><Link to='/projects'>Projects</Link></Navbar.Link>
-        <Navbar.Link active={path === "/contact"}><Link to='/contact'>Contact</Link></Navbar.Link>        
+        <Navbar.Link active={path === "/about"} as={'div'}><Link to='/about'>About</Link></Navbar.Link>
+        <Navbar.Link active={path === "/projects"} as={'div'}><Link to='/projects'>Projects</Link></Navbar.Link>
+        <Navbar.Link active={path === "/contact"} as={'div'}><Link to='/contact'>Contact</Link></Navbar.Link>        
         </div>
 
         <div>

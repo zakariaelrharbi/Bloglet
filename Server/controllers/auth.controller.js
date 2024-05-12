96e8d7a7bb1b2355e137babac98baa6376045637
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-05-13 00:19:40 +0100

    handlling errors and hashing password

--- Server/controllers/auth.controller.js
+++ Server/controllers/auth.controller.js
@@ -1,4 +1,8 @@
 const User = require('../models/user.model');
+const bcrypt = require('bcrypt');
+
+
+
 const signup = async (req, res) => {
     const { username, email, password } = req.body;
     if (!username || !email || !password ||
@@ -6,10 +10,15 @@ const signup = async (req, res) => {
     {
         return res.status(400).json({ message: 'All fields are required' });
     }
-    const newUser = new User({ username, email, password });
 
-    await newUser.save();
-    res.status(200).json({ message: 'User created successfully' });
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword});
+    try {
+        await newUser.save();
+        res.status(200).json({ message: 'User created successfully' });        
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 };
 
 

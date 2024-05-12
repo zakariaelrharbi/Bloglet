23e86adb2ccc5b5ed6421a7e6b1fcca1aa2ddc9c
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-05-12 19:48:27 +0100

    mongoose installed

--- Server/app.js
+++ Server/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const connect = require('./config/database');
+const dotenv = require("dotenv");
+
+
+dotenv.config();
+
 const app = express();
+const PORT = process.env.SERVER_PORT || 5000;
 
-const PORT = 4000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+async function main() {
+    await connect();
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+main();

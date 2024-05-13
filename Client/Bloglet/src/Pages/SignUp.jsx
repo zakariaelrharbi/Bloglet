4be802ecb82941bfac6998ed1762054258f9b886
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-05-13 11:28:22 +0100

    password and form validation added

--- Client/Bloglet/src/Pages/SignUp.jsx
+++ Client/Bloglet/src/Pages/SignUp.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
-import { FaUserAlt } from "react-icons/fa";
+import React, { useState } from 'react';
+import { FaUserAlt, FaRegEye, FaRegEyeSlash } from "react-icons/fa";
+import { MdOutlineMail } from "react-icons/md";
+import { useForm } from 'react-hook-form';
+
 
 
 
 export default function SignUp() {
+  const { register, handleSubmit, formState: { errors, isValid }, watch } = useForm({
+        defaultValues: {
+            username: "",
+            email: "",
+            password: ""
+        },
+        mode: "onChange",
+    });
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [termsAccepted, setTermsAccepted] = useState(false);
+
+  const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
   return (
     <>
      <div className="font-[sans-serif] text-gray-800 bg-white max-w-4xl flex items-center mx-auto md:h-screen p-4">
@@ -28,7 +47,7 @@ export default function SignUp() {
         </p>
       </div>
     </div>
-    <form className="md:col-span-2 w-full py-6 px-6 sm:px-16">
+    <form className="md:col-span-2 w-full py-6 px-6 sm:px-16" onSubmit={handleSubmit}>
       <div className="mb-6">
         <h3 className="text-2xl font-bold">Create an account</h3>
       </div>
@@ -39,12 +58,24 @@ export default function SignUp() {
             <input
               name="username"
               type="text"
-              required=""
+              {...register("username",{
+                    minLength: {
+                        value: 3,
+                        message: "username must be at least 3 character",
+                    },
+                    required:{
+                        value: true,
+                        message: "username is required"
+                    }
+                })}
               className="bg-white border border-gray-300 w-full text-sm px-4 py-2.5 rounded-md focus:border-cyan-500 focus:ring-cyan-500"
               placeholder="Enter username"
             />
-            <FaUserAlt className="w-4 h-4 absolute right-4" />
+            <FaUserAlt className="w-4 h-4 absolute right-4 opacity-60" />
           </div>
+            {errors.username?.message && (
+                <p className="text-red-500 text-xs mt-1 block text-left">{errors.username?.message}</p>
+              )}
         </div>
         <div>
           <label className="text-sm mb-2 block">Email</label>
@@ -52,64 +83,59 @@ export default function SignUp() {
             <input
               name="email"
               type="email"
-              required=""
-              className="bg-white border border-gray-300 w-full text-sm px-4 py-2.5 rounded-md outline-blue-500"
+              {...register("email", {
+                   required:{
+                        value: true,
+                        message: "Email is required"
+                    },
+                    pattern: {
+                    value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                    message: 'Please enter a valid email',
+                    }
+                })}
+              className="bg-white border border-gray-300 w-full text-sm px-4 py-2.5 rounded-md focus:border-cyan-500 focus:ring-cyan-500"
               placeholder="example@gmail.com"
             />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="#bbb"
-              stroke="#bbb"
-              className="w-4 h-4 absolute right-4"
-              viewBox="0 0 682.667 682.667"
-            >
-              <defs>
-                <clipPath id="a" clipPathUnits="userSpaceOnUse">
-                  <path d="M0 512h512V0H0Z" data-original="#000000" />
-                </clipPath>
-              </defs>
-              <g
-                clipPath="url(#a)"
-                transform="matrix(1.33 0 0 -1.33 0 682.667)"
-              >
-                <path
-                  fill="none"
-                  strokeMiterlimit={10}
-                  strokeWidth={40}
-                  d="M452 444H60c-22.091 0-40-17.909-40-40v-39.446l212.127-157.782c14.17-10.54 33.576-10.54 47.746 0L492 364.554V404c0 22.091-17.909 40-40 40Z"
-                  data-original="#000000"
-                />
-                <path
-                  d="M472 274.9V107.999c0-11.027-8.972-20-20-20H60c-11.028 0-20 8.973-20 20V274.9L0 304.652V107.999c0-33.084 26.916-60 60-60h392c33.084 0 60 26.916 60 60v196.653Z"
-                  data-original="#000000"
-                />
-              </g>
-            </svg>
+            <MdOutlineMail className="w-5 h-5 absolute right-4 opacity-60" />
           </div>
+           {errors.email?.message && (
+                <p className="text-red-500 text-xs mt-1 block text-left">{errors.email?.message}</p>
+            )}
         </div>
         <div>
           <label className="text-sm mb-2 block">Password</label>
           <div className="relative flex items-center">
             <input
               name="password"
-              type="password"
-              required=""
-              className="bg-white border border-gray-300 w-full text-sm px-4 py-2.5 rounded-md outline-blue-500"
+              type={showPassword ? 'text' : 'password'}
+              {...register("password",{
+                    minLength: {
+                        value: 8,
+                        message: "Password must be at least 8 character",
+                    },
+                    required:{
+                        value: true,
+                        message: "Password is required"
+                    },
+                })}
+              className="bg-white border border-gray-300 w-full text-sm px-4 py-2.5 rounded-md focus:border-cyan-500 focus:ring-cyan-500"
               placeholder="Enter password"
             />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="#bbb"
-              stroke="#bbb"
-              className="w-4 h-4 absolute right-4 cursor-pointer"
-              viewBox="0 0 128 128"
-            >
-              <path
-                d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z"
-                data-original="#000000"
-              />
-            </svg>
+            {showPassword ? (
+                <FaRegEyeSlash
+                  className="w-4 h-4 absolute right-4 cursor-pointer opacity-60"
+                  onClick={togglePasswordVisibility}
+                />
+              ) : (
+                <FaRegEye
+                  className="w-4 h-4 absolute right-4 cursor-pointer opacity-60"
+                  onClick={togglePasswordVisibility}
+                />
+              )}
           </div>
+          {errors.password?.message && (
+                <p className="text-red-500 text-xs mt-1 block text-left">{errors.password?.message}</p>
+            )}
         </div>
         <div className="flex items-center">
           <input
@@ -117,11 +143,11 @@ export default function SignUp() {
             name="remember-me"
             type="checkbox"
             className="h-4 w-4 shrink-0 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
+            onChange={(e) => setTermsAccepted(e.target.checked)}
           />
           <label htmlFor="remember-me" className="ml-3 block text-sm">
             I accept the{" "}
             <a
-              href="javascript:void(0);"
               className="text-blue-600 font-semibold hover:underline ml-1"
             >
               Terms and Conditions
@@ -132,7 +158,8 @@ export default function SignUp() {
       <div className="!mt-10">
         <button
           type="button"
-          className="w-full py-3 px-4 text-sm font-semibold rounded text-white bg-gray-700 hover:bg-gray-800 focus:outline-none"
+          disabled = {!isValid || !termsAccepted}
+          className="w-full py-3 px-4 text-sm font-semibold rounded text-white bg-gray-700 hover:bg-gray-800 focus:outline-none cursor-pointer disabled:opacity-70 disabled:cursor-not-allowed"
         >
           Create an account
         </button>

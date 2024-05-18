d04a25568b9504804fccfdb8281f7fc6b60de705
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-05-18 17:53:28 +0100

    updating success  message

--- Server/controllers/auth.controller.js
+++ Server/controllers/auth.controller.js
@@ -3,18 +3,14 @@ const bcrypt = require('bcrypt');
 const errorHandler = require('../utils/error');
 const jwt = require('jsonwebtoken');
 
-
-
 const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
-    if (!username || !email || !password ||
-        username === '' || email === '' || password === '' )
-    {
-        next(errorHandler(400, 'All fields are required'));    
+    if (!username || !email || !password || username === '' || email === '' || password === '') {
+        return next(errorHandler(400, 'All fields are required'));    
     }
 
     const hashedPassword = bcrypt.hashSync(password, 10);
-    const newUser = new User({ username, email, password: hashedPassword});
+    const newUser = new User({ username, email, password: hashedPassword });
     try {
         await newUser.save();
         res.status(200).json({ message: 'User created successfully' });        
@@ -26,29 +22,29 @@ const signup = async (req, res, next) => {
 const signin = async (req, res, next) => {
     const { email, password } = req.body;
     if (!email || !password || email === '' || password === '') {
-        next(errorHandler(400, 'All fields are required'));
+        return next(errorHandler(400, 'All fields are required'));
     }
 
     try {
         const ValidUser = await User.findOne({ email });
         if (!ValidUser) {
-            next(errorHandler(404, 'User not found'));
+            return next(errorHandler(404, 'User not found'));
         }
         const ValidPassword = bcrypt.compareSync(password, ValidUser.password);
         if (!ValidPassword) {
             return next(errorHandler(401, 'Invalid Password'));
         }
-        res.status(200).json({ message: 'User signed in successfully' });
 
         const token = jwt.sign({ id: ValidUser._id }, process.env.JWT_SECRET, { expiresIn: '2h' });
-        res.status(200).cookie('access_token', token, { httpOnly: true }).json({ ValidUser });
+        const { password: userPassword, ...rest } = ValidUser._doc;
+
+        res.status(200)
+           .cookie('access_token', token, { httpOnly: true })
+           .json({ message: 'User signed in successfully', rest});
 
     } catch (error) {
         next(error);
     }
 };
-
-
-
 
 module.exports = { signup, signin };

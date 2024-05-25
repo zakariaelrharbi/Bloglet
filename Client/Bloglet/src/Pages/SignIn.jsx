dba6eec8fb3079a3c5c61bb0c9cfe2c7cf03dd45
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-05-25 14:26:50 +0100

    updating sign in page

--- Client/Bloglet/src/Pages/SignIn.jsx
+++ Client/Bloglet/src/Pages/SignIn.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
+import { MdOutlineMail } from "react-icons/md";
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom' 
+
+
+
+
+
 export default function SignIn() {
+  const { register, handleSubmit, formState: { errors, isValid } } = useForm({
+    defaultValues: {
+      username: "",
+      email: "",
+      password: ""
+    },
+    mode: "onChange",
+  });
+
+  const [showPassword, setShowPassword] = useState(false);
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <>
       <div className="font-[sans-serif] text-gray-800 bg-white max-w-4xl flex items-center mx-auto md:h-screen p-4">
@@ -13,64 +35,57 @@ export default function SignIn() {
           of possibilities. Your journey begins here.
         </p>
       </div>
-      <div className="relative flex items-center">
-        <label className="text-[13px] bg-white absolute px-2 top-[-10px] left-[18px] font-semibold">
-          Email
-        </label>
-        <input
-          type="email"
-          placeholder="Enter email"
-          className="px-4 py-3.5 bg-white w-full text-sm border-2 border-gray-200 focus:border-blue-600 rounded outline-none"
-        />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="#bbb"
-          stroke="#bbb"
-          className="w-[18px] h-[18px] absolute right-4"
-          viewBox="0 0 682.667 682.667"
-        >
-          <defs>
-            <clipPath id="a" clipPathUnits="userSpaceOnUse">
-              <path d="M0 512h512V0H0Z" data-original="#000000" />
-            </clipPath>
-          </defs>
-          <g clipPath="url(#a)" transform="matrix(1.33 0 0 -1.33 0 682.667)">
-            <path
-              fill="none"
-              strokeMiterlimit={10}
-              strokeWidth={40}
-              d="M452 444H60c-22.091 0-40-17.909-40-40v-39.446l212.127-157.782c14.17-10.54 33.576-10.54 47.746 0L492 364.554V404c0 22.091-17.909 40-40 40Z"
-              data-original="#000000"
-            />
-            <path
-              d="M472 274.9V107.999c0-11.027-8.972-20-20-20H60c-11.028 0-20 8.973-20 20V274.9L0 304.652V107.999c0-33.084 26.916-60 60-60h392c33.084 0 60 26.916 60 60v196.653Z"
-              data-original="#000000"
-            />
-          </g>
-        </svg>
-      </div>
-      <div className="relative flex items-center mt-10">
-        <label className="text-[13px] bg-white absolute px-2 top-[-10px] left-[18px] font-semibold">
-          Password
-        </label>
-        <input
-          type="password"
-          placeholder="Enter password"
-          className="px-4 py-3.5 bg-white w-full text-sm border-2 border-gray-200 focus:border-blue-600 rounded outline-none"
-        />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="#bbb"
-          stroke="#bbb"
-          className="w-[18px] h-[18px] absolute right-4 cursor-pointer"
-          viewBox="0 0 128 128"
-        >
-          <path
-            d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z"
-            data-original="#000000"
-          />
-        </svg>
-      </div>
+      <div>
+                <label className="text-sm mb-2 block">Email</label>
+                <div className="relative flex items-center">
+                  <input
+                    {...register("email", {
+                      required: "Email is required",
+                      pattern: {
+                        value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                        message: 'Please enter a valid email',
+                      }
+                    })}
+                    className="bg-white border border-gray-300 w-full text-sm px-4 py-2.5 mb-1 rounded-md focus:border-cyan-500 focus:ring-cyan-500 focus:outline-none"
+                    placeholder="example@gmail.com"
+                  />
+                  <MdOutlineMail className="w-5 h-5 absolute right-4 top-2.5 opacity-60 " />
+                </div>
+                {errors.email && (
+                  <p className="text-red-500 text-xs mb-4 block">{errors.email.message}</p>
+                )}
+              </div>
+              <div>
+                <label className="text-sm mb-2 block">Password</label>
+                <div className="relative flex items-center">
+                  <input
+                    {...register("password", {
+                      minLength: {
+                        value: 8,
+                        message: "Password must be at least 8 characters",
+                      },
+                      required: "Password is required",
+                    })}
+                    type={showPassword ? 'text' : 'password'}
+                    className="bg-white border border-gray-300 w-full text-sm px-4 py-2.5 rounded-md focus:border-cyan-500 focus:ring-cyan-500 focus:outline-none"
+                    placeholder="Enter password"
+                  />
+                  {showPassword ? (
+                    <FaRegEyeSlash
+                      className="w-4 h-4 absolute right-4 cursor-pointer opacity-60"
+                      onClick={togglePasswordVisibility}
+                    />
+                  ) : (
+                    <FaRegEye
+                      className="w-4 h-4 absolute right-4 cursor-pointer opacity-60"
+                      onClick={togglePasswordVisibility}
+                    />
+                  )}
+                </div>
+                {errors.password && (
+                  <p className="text-red-500 text-xs mt-1 block">{errors.password.message}</p>
+                )}
+              </div>
       <div className="flex items-center justify-between gap-2 mt-6">
         <div className="flex items-center">
           <input

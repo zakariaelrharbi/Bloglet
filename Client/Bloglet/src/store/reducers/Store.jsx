4fa48ad491676c77bc542fde605fdfe3f1e0fb77
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-06-29 13:35:58 +0100

    removing store file and adding redux

--- Client/Bloglet/src/store/reducers/Store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./auth"; // Import your authSlice reducer
-
-export default configureStore({
-  reducer: {
-    auth: authReducer, // Pass your authSlice reducer under the 'auth' key
-  },
-});

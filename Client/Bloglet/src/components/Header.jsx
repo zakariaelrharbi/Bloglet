e85e2b7aa02139e625577845e44c1387bcce8479
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-07-03 16:18:41 +0100

    update header

--- Client/Bloglet/src/components/Header.jsx
+++ Client/Bloglet/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { TextInput } from 'flowbite-react';
 import { AiOutlineSearch } from "react-icons/ai";

2677eb83ece9924ebdeb940f52d92768c61aa4f1
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-07-03 13:44:49 +0100

    adding validation to the user schema

--- Server/models/userModel.js
+++ Server/models/userModel.js
@@ -3,17 +3,25 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
-        required: true,
+        required: [true, 'Username is required'],
         unique: true,
     },
     email: {
         type: String,
-        required: true,
+        required: [true, 'Email is required'],
         unique: true,
+        match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address']
     },
     password: {
         type: String,
-        required: true,
+        required: [true, 'Password is required'],
+        validate: {
+            validator: function(value) {
+                // Minimum 8 characters, at least one uppercase letter
+                return /^(?=.*[A-Z]).{8,}$/.test(value);
+            },
+            message: 'Password must be at least 8 characters long and contain at least one uppercase letter'
+        }
     },
     profilePicture: {
         type: String,

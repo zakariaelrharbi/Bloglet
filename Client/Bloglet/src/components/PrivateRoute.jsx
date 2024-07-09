e5501387568591b72404c0f37da1c767771b9bc2
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-07-09 23:40:43 +0100

    make the dashboad private

--- Client/Bloglet/src/components/PrivateRoute.jsx
+++ Client/Bloglet/src/components/PrivateRoute.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import { useSelector } from 'react-redux'
+import { Outlet, Navigate } from 'react-router-dom'
 
 const PrivateRoute = () => {
+    const {currentUser} = useSelector((state) => state.user);
   return (
-    <div>
-      
-    </div>
+    <>
+      {currentUser ? <Outlet /> : <Navigate to='/sign-in' />}
+    </>
   )
 }
 

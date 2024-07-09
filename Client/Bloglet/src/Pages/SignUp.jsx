a778ee3e585dc18b397aba76cd5b2e1c9d5fe043
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-07-09 18:01:41 +0100

    adding loaading

--- Client/Bloglet/src/Pages/SignUp.jsx
+++ Client/Bloglet/src/Pages/SignUp.jsx
@@ -65,6 +65,7 @@ export default function SignUp() {
     setErrors({});
 
     try {
+      setLoading(true);
       const response = await fetch('http://localhost:5000/api/signup', {
         method: 'POST',
         headers: {

ce49420767c80defebab285ff6e7f6c879aa5121
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-07-10 12:32:12 +0100

    working on dashboard

--- Client/Bloglet/src/Pages/Dashboard.jsx
+++ Client/Bloglet/src/Pages/Dashboard.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useLocation } from 'react-router-dom'
 
 export default function Dashboard() {
+  const location = useLocation()
+  const [tab, setTab] = useState('')
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search)
+    const tabFromUrl = urlParams.get('tab')
+    console.log(tabFromUrl)
+  }, [location])
   return (
     <div>
       <h1>dashboard</h1>

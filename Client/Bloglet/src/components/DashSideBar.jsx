f6e97050ff53d89db808b087ad19d85c2bf4a3bd
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-07-18 16:28:01 +0100

    updating side bar

--- Client/Bloglet/src/components/DashSideBar.jsx
+++ Client/Bloglet/src/components/DashSideBar.jsx
@@ -1,19 +1,33 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {Sidebar} from 'flowbite-react';
 import { SlUser } from "react-icons/sl";
 import { PiSignOutLight } from "react-icons/pi";
+import { Link, useLocation } from 'react-router-dom'
+
+
 
 
 
 const DashSideBar = () => {
+  const location = useLocation()
+  const [tab, setTab] = useState('')
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search)
+    const tabFromUrl = urlParams.get('tab')
+    if (tabFromUrl) {
+      setTab(tabFromUrl)
+    }
+  }, [location.search])
   return (
     <Sidebar>
       <Sidebar.Items>
         <Sidebar.ItemGroup className='cursor-pointer'>
-          <Sidebar.Item active icon={SlUser} label={"User"} labelColor ='dark'>
-            Profile
-          </Sidebar.Item>
-          <Sidebar.Item active icon={PiSignOutLight} >
+          <Link to='/dashboard?tab=profile'>
+            <Sidebar.Item active={tab === 'profile'} icon={SlUser} label={"User"} labelColor ='dark'>
+              Profile
+            </Sidebar.Item>
+          </Link>
+          <Sidebar.Item  icon={PiSignOutLight} >
             Log Out
           </Sidebar.Item>
         </Sidebar.ItemGroup>

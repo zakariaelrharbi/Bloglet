96cac9b75fc7dbff1d632ae793d87147ca17a401
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-07-27 00:36:46 +0100

    adding dashprofile

--- Client/Bloglet/src/components/DashProfile.jsx
+++ Client/Bloglet/src/components/DashProfile.jsx
@@ -1,13 +1,25 @@
 import { Button, TextInput } from 'flowbite-react'
-import React from 'react'
+import React, {useState} from 'react'
 import { useSelector } from 'react-redux'
 
 const DashProfile = () => {
   const currentUser = useSelector((state) => state.user.currentUser)
+  const [uploadImage, setUploadImage] = useState(null)
+  const [ imageFileUrl, setImageFileUrl ] = useState(null)
+  const handleImageUpload = (e) => {
+    const file = e.target.files[0]
+    if(file){
+      setUploadImage(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  }
+  console.log(uploadImage, imageFileUrl)
+
   return (
     <div className='max-w-lg mx-auto p-3'>
       <h1 className='mb-3 text-center font-semibold text-3xl'>Profile</h1>
       <form className='flex flex-col gap-4'>
+        <input type='file' accept='image/*' onChange={handleImageUpload} className='' />
         <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'>
           <img src={currentUser.profilePicture} alt="user" 
           className='rounded-full w-full h-full object-cover border-8 border-[#acacab]' />

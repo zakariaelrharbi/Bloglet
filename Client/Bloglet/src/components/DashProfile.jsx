9627dd8927eb99fab7e7078461911c3dfd036707
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-07-26 23:34:51 +0100

    adding dashprofile

--- Client/Bloglet/src/components/DashProfile.jsx
+++ Client/Bloglet/src/components/DashProfile.jsx
@@ -1,4 +1,4 @@
-import { TextInput } from 'flowbite-react'
+import { Button, TextInput } from 'flowbite-react'
 import React from 'react'
 import { useSelector } from 'react-redux'
 
@@ -7,15 +7,19 @@ const DashProfile = () => {
   return (
     <div className='max-w-lg mx-auto p-3'>
       <h1 className='mb-3 text-center font-semibold text-3xl'>Profile</h1>
-      <form className='flex flex-col'>
+      <form className='flex flex-col gap-4'>
         <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'>
           <img src={currentUser.profilePicture} alt="user" 
           className='rounded-full w-full h-full object-cover border-8 border-[#acacab]' />
         </div>
         <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username}/>
         <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email}/>
-        <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username}/>
+        <TextInput type='password' id='password' placeholder='password'/>
+        <Button type='submit' className=''>Update</Button>
       </form>
+      <div>
+        
+      </div>
     </div>
   )
 }

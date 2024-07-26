2e91e9c45b721b402318ac9d7d620e5d76c30f16
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-07-26 23:36:39 +0100

    adding dashprofile

--- Client/Bloglet/src/components/DashProfile.jsx
+++ Client/Bloglet/src/components/DashProfile.jsx
@@ -17,8 +17,9 @@ const DashProfile = () => {
         <TextInput type='password' id='password' placeholder='password'/>
         <Button type='submit' className=''>Update</Button>
       </form>
-      <div>
-        
+      <div className='text-red-500 flex justify-between mt-5'>
+        <span className='cursor-pointer'>Delete Account</span>
+        <span className='cursor-pointer'>Sign Out</span>
       </div>
     </div>
   )

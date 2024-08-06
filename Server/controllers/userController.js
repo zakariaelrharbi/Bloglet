e0e0cec4a0bfea76b57bd9cb2b8c33c60a4233ba
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-08-06 16:51:48 +0100

    creating middleware to verify if the user is auth

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -4,5 +4,9 @@ const User = require('../models/userModel');
 
 // update user info
 const updateUserInfo = async (req, res) => {
-    
+    const { userId } = req.params;
+    const { username, email, password, profilePicture } = req.body;
+
+    try {
+        const user = await User.findById(userId);
 }
---
+++ Server/middleware/verifyUser.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+
+
+// Middleware to verify user
+const verifyUser = (req, res, next) => {
+    const token = req.cookies.access_token;
+    if (!token) {
+        return res.status(401).json({
+            message: 'Access denied',
+            error: true,
+            success: false,
+        });
+    }
+    try {
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = verified;
+        next();
+    } catch (error) {
+        return res.status(400).json({
+            message: 'Invalid token',
+            error: true,
+            success: false,
+        });
+    }
+};

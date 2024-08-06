e6bef5a1e21f968c9b0e8276a4d703230f5ade73
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-08-06 19:41:25 +0100

    adding slice fo update function

--- Client/Bloglet/src/redux/user/userSlice.js
+++ Client/Bloglet/src/redux/user/userSlice.js
@@ -28,6 +28,19 @@ const initialState = {
             state.error = null;
             state.loading = false;
         },
+        updateStart: (state) => {
+            state.loading = true;
+            state.error = null;
+        },
+        updateSuccess: (state, action) => {
+            state.currentUser = action.payload;
+            state.loading = false;
+            state.error = null;
+        },
+        updateFailure: (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+        },
     },
 });
 

1b8a80c1cc9955f837fe7115eb4045e42cace812
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-08-25 14:58:21 +0100

    adding is admin to sign in

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -90,7 +90,7 @@ const userSignin = async (req, res) => {
             });
         }
 
-        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        const token = jwt.sign({ id: validUser._id, isAmin: validUser.isAdmin }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
         const { password: userPassword, ...rest } = validUser._doc;
         return res.status(200)
@@ -117,7 +117,7 @@ const google = async (req, res) => {
     try {
         const user = await User.findOne({ email });
         if (user) {
-            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+            const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin}, process.env.JWT_SECRET);
             const { password, ...rest } = user._doc;
             return res.status(200)
                 .cookie('access_token', token, { httpOnly: true, secure: process.env.NODE_ENV === 'production' })
@@ -138,7 +138,7 @@ const google = async (req, res) => {
                 profilePicture: googlePhotoURL,
             });
             await newUser.save();
-            const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+            const token = jwt.sign({ id: newUser._id, isAdmin: newUser.isAdmin}, process.env.JWT_SECRET);
             const { password, ...rest } = newUser._doc;
             return res.status(200)
                 .cookie('access_token', token, { httpOnly: true, secure: process.env.NODE_ENV === 'production' })

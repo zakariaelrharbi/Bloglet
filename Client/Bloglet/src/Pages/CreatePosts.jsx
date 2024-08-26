543d77311fa0830d0a2e09d182d62deb1bed1dc8
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-08-26 22:58:00 +0100

    start adding form

--- Client/Bloglet/src/Pages/CreatePosts.jsx
+++ Client/Bloglet/src/Pages/CreatePosts.jsx
@@ -1,9 +1,18 @@
+import { TextInput } from 'flowbite-react'
 import React from 'react'
 
 const CreatePost = () => {
   return (
     <div className='p-3 max-w-3xl mx-auto min-h-screen'>
       <h1 className='text-3xl text-center my-7 font-semibold'>Create a post</h1>
+      <form className='flex flex-col gap-4'>
+        <div className='flex flex-col gap-4 sm:flex-row justify-between'>
+            <TextInput type='text' placeholder='Title' required id='title'
+            className='flex-1'>
+
+            </TextInput>
+        </div>
+      </form>
     </div>
   )
 }

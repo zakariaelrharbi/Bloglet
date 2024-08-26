80f243d7e2fe319c629cb1c2d8d1bc2a29f10f5a
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-08-26 22:51:47 +0100

    adding text before form

--- Client/Bloglet/src/Pages/CreatePosts.jsx
+++ Client/Bloglet/src/Pages/CreatePosts.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CreatePost = () => {
   return (
     <div className='p-3 max-w-3xl mx-auto min-h-screen'>
-      Create post
+      <h1 className='text-3xl text-center my-7 font-semibold'>Create a post</h1>
     </div>
   )
 }

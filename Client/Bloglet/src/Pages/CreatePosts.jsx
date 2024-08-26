16cd14d07cfa75b44af5bd59a7fb1808cbb6aa89
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-08-26 22:50:00 +0100

    starting ui of create post

---
+++ Client/Bloglet/src/Pages/CreatePosts.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreatePost = () => {
+  return (
+    <div className='p-3 max-w-3xl mx-auto min-h-screen'>
+      Create post
+    </div>
+  )
+}
+
+export default CreatePost

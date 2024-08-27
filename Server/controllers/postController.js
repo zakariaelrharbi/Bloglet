a0e3c21eb4aea33217a65f90d3d60fda64c357e8
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-08-28 00:42:07 +0100

    get all post function done

--- Server/controllers/postController.js
+++ Server/controllers/postController.js
@@ -36,7 +36,54 @@ const createPost = async (req, res) => {
     }
 };
 
-const getPosts = async (req, res) => {};
+const getPosts = async (req, res) => {
+    try {
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        const limit = parseInt(req.query.limit) || 9;
+        const sortDirection = req.query.order === 'asc' ? 1 : -1;
+        const posts = await Post.find({
+          ...(req.query.userId && { userId: req.query.userId }),
+          ...(req.query.category && { category: req.query.category }),
+          ...(req.query.slug && { slug: req.query.slug }),
+          ...(req.query.postId && { _id: req.query.postId }),
+          ...(req.query.serchTerm && {
+            $or: [
+                { title: { $regex: req.query.searchTerm, $options: 'i' } },
+                { content: { $regex: req.query.searchTerm, $options: 'i' } },
+                ],
+            }),
+        })
+            .sort({ createdAt: sortDirection })
+            .skip(startIndex)
+            .limit(limit);
+
+        const totalPost = await Post.countDocuments();
+        const now = new Date();
+        
+        const oneMonthAgo = new Date(
+            now.getFullYear(),
+            now.getMonth() - 1,
+            now.getDate()
+        );
+        const lastMonthPosts = await Post.countDocuments({
+            createdAt: { $gte: oneMonthAgo },
+        });
+        return res.status(200).json({
+            posts,
+            totalPost,
+            lastMonthPosts,
+            message: 'Posts fetched successfully',
+            error: false,
+            success: true
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message,
+            error: true,
+            success: false
+        });
+    }
+};
 
 const updatePost = async (req, res) => {};
 

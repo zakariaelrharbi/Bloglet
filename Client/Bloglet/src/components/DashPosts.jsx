de186b0e517c3fd69eccfc0eb582cb1bbcd00b8a
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-08-28 21:59:24 +0100

    add delete function

--- Client/Bloglet/src/components/DashPosts.jsx
+++ Client/Bloglet/src/components/DashPosts.jsx
@@ -1,15 +1,20 @@
-import { Table } from 'flowbite-react'
+import { Table, Modal, Button } from 'flowbite-react'
 import React, {useEffect, useState} from 'react'
 import { useSelector} from 'react-redux'
 import { Link } from 'react-router-dom';
 import { toast } from 'sonner';
+import { HiOutlineExclamationCircle } from "react-icons/hi";
+import { useNavigate } from 'react-router-dom';
+
 
 const DashPosts = () => {
 
   const { currentUser } = useSelector((state) => state.user)
   const [userPosts, setUserPosts] = useState([])
   const [showMore, setShowMore] = useState(true)
-  console.log(userPosts)
+  const navigate = useNavigate()
+  const [showModal, setShowModal] = useState(false)
+  const [postIdToDelete, setPostIdToDelete] = useState('')
   useEffect(() => {
     const fetchPosts = async () => {
       try {
@@ -46,6 +51,30 @@ const DashPosts = () => {
       toast.error('An error occurred while fetching posts')
     }
   }
+
+  // Function to handle delete post
+  const handleDeletePost = async () => {
+    setShowModal(false)
+    try {
+      const res = await fetch(`http://localhost:5000/api/post/deletePost/${postIdToDelete}/${currentUser._id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        credentials: 'include',
+      })
+      const data = await res.json()
+      if(!res.ok) {
+        toast.error(data.message)
+      }else {
+        toast.success(data.message)
+        setUserPosts((prevPosts) => prevPosts.filter((post) => post._id !== postIdToDelete))
+        navigate('/dashboard?tab=posts')
+      }
+    } catch (error) {
+      toast.error('An error occurred while deleting post')
+    }
+  }
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto  scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
       { userPosts.length > 0 ? (
@@ -78,7 +107,13 @@ const DashPosts = () => {
                   </Table.Cell>
                   <Table.Cell>{post.category}</Table.Cell>
                   <Table.Cell>
-                    <button className='bg-red-500 text-white px-2 py-1 rounded-md'>Delete</button>
+                    <button 
+                      onClick={() => {
+                        setShowModal(true)
+                        setPostIdToDelete(post._id)
+                      }
+                      }
+                     className='bg-red-500 text-white px-2 py-1 rounded-md'>Delete</button>
                   </Table.Cell>
                   <Table.Cell>
                     <Link to={`/update-post/${post._id}`}>
@@ -101,6 +136,24 @@ const DashPosts = () => {
         </div>
       )
       }
+      <Modal 
+        show={showModal} 
+        onClose={()=>setShowModal(false)}
+        popup
+        size='md'
+        >
+        <Modal.Header/> {/* Modal header */}
+        <Modal.Body> {/* Modal body */}
+          <div className='text-center'>
+            <HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto'/> {/* Icon */}
+            <h3 className='text-lg font-semibold mb-5 text-gray-500 dark:text-gray-400'>Are you sure you want to delete this post?</h3> {/* Confirmation message */}
+            <div className='flex justify-center gap-4'> 
+              <Button color='failure' className='mr-2' onClick={handleDeletePost}>Yes, I'm sure</Button> {/* Button to cancel account deletion */}
+              <Button onClick={()=>setShowModal(false)}>No, Cancel</Button> {/* Button to confirm account deletion */}
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal> 
     </div>
   )
 }

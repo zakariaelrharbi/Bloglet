36a32c64d61f56b619f0037d3ddaf047966971c1
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-11-22 23:12:37 +0100

    add more padding and margin

--- Client/Bloglet/src/components/OurBlog.jsx
+++ Client/Bloglet/src/components/OurBlog.jsx
@@ -26,12 +26,12 @@ const OurBlog = () => {
   }, []);
 
   return (
-    <div className="bg-white font-[sans-serif] my-4">
+    <div className="bg-white font-[sans-serif] my-6">
       <div className="max-w-7xl mx-auto">
         {posts && posts.length > 0 && (
           <>
             <div className="text-center">
-              <h2 className="text-3xl font-extrabold text-[#333] inline-block relative after:absolute after:w-4/6 after:h-1 after:left-0 after:right-0 after:-bottom-4 after:mx-auto after:bg-pink-400 after:rounded-full">
+              <h2 className="text-3xl py-2 font-extrabold text-[#333] inline-block relative after:absolute after:w-4/6 after:h-1 after:left-0 after:right-0 after:-bottom-4 after:mx-auto after:bg-pink-400 after:rounded-full">
                 LATEST BLOGS
               </h2>
             </div>

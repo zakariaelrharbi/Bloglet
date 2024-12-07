dae1cba7cc79732fef34e7fc0423ef7cc6c926d4
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-12-07 13:33:36 +0100

    hide fire base key

---
+++ Client/src/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "bloglet-68705.firebaseapp.com",
+  projectId: "bloglet-68705",
+  storageBucket: "bloglet-68705.appspot.com",
+  messagingSenderId: "74199048353",
+  appId: "1:74199048353:web:a66f3ed0db38ae513250e0",
+  measurementId: "G-JSTF6XWKP6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Export the app
+export { app };
